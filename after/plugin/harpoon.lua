28eefbca429f70b444642898c1639b92e4c1c6ca
m8-XD <m8-XD@proton.me>
2024-03-09 14:22:28 +0300

    change harpoon ui

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,27 +2,9 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
--- basic telescope configuration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
-
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
-end
-
 vim.keymap.set("n", "<leader>ja", function() harpoon:list():append() end,
     { desc = "Add current file to harpoon list" })
-vim.keymap.set("n", "<leader>je", function() toggle_telescope(harpoon:list()) end,
+vim.keymap.set("n", "<leader>je", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
     { desc = "Open harpoon window" })
 
 vim.keymap.set("n", "<leader>j1", function() harpoon:list():select(1) end,
