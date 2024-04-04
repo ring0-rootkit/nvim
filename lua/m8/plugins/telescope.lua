8b93f4e6be6cd9be46c70524b67e97c35b3ad7a0
m8-XD <m8-XD@proton.me>
2024-04-04 14:34:03 +0300

    make telescope search result to be 'smart' (truncate file name if it is long)

--- lua/m8/plugins/telescope.lua
+++ lua/m8/plugins/telescope.lua
@@ -14,6 +14,7 @@ return {
     config = function ()
         require('telescope').setup {
           defaults = {
+            path_display = {"smart"},
             mappings = {
               i = {
                 ['<C-u>'] = false,
