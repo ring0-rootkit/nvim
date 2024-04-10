0d94c2860dcbe735194c588221e04b136cfd3675
m8-XD <ord1naryman.dmitry@gmail.com>
2024-04-11 01:59:16 +0300

    change theme

--- lua/m8/plugins/theme.lua
+++ lua/m8/plugins/theme.lua
@@ -1,13 +1,3 @@
-  return {
-    -- {
-    --     "catppuccin/nvim",
-    --     name = "catppuccin",
-    --     priority = 1000,
-    -- },
-    {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
-    }
-  }
+return {
+    { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+}
