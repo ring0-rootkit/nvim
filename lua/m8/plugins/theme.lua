36a212365ac6e399d290892105649e75b2bd8109
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-05-06 16:28:11 +0300

    add tab

--- lua/m8/plugins/theme.lua
+++ lua/m8/plugins/theme.lua
@@ -1,15 +1,15 @@
 return {
     {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    init = function()
-        require("rose-pine").setup({
-            -- variant = "dawn",
-            styles = {
-                transparency = true,
-            },
-        })
-    end,
+        "rose-pine/neovim",
+        name = "rose-pine",
+        init = function()
+            require("rose-pine").setup({
+                -- variant = "dawn",
+                styles = {
+                    transparency = true,
+                },
+            })
+        end,
     },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
