fd800b280beff3ca8dbcf1344eaf9dd57db0c159
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-05-31 00:08:23 +0300

    fix: enable oil and a little cleanup

--- init.lua
+++ init.lua
@@ -97,8 +97,6 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
 			vim.keymap.set("n", "<leader>/", function()
@@ -151,15 +149,7 @@ require("lazy").setup({
 				gopls = {},
 				rust_analyzer = {},
 				tsserver = {},
-				lua_ls = {
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				},
+				lua_ls = {},
 			}
 
 			require("mason").setup()
@@ -225,6 +215,7 @@ require("lazy").setup({
 	},
 	{
 		"stevearc/oil.nvim",
+		opts = {},
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 })
