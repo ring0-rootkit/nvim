c6e885eaaba0507fac06de57137b9479a7f4c1da
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-07-08 11:03:26 +0300

    feat: add multicursor plugin

--- lua/ring0/plugins/ui.lua
+++ lua/ring0/plugins/ui.lua
@@ -26,11 +26,8 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
+		opts = {},
 		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
 		},
 		setup = function()
@@ -48,7 +45,6 @@ return {
 					bottom_search = true, -- use a classic bottom cmdline for search
 					command_palette = true, -- position the cmdline and popupmenu together
 					long_message_to_split = true, -- long messages will be sent to a split
-					inc_rename = false, -- enables an input dialog for inc-rename.nvim
 					lsp_doc_border = false, -- add a border to hover docs and signature help
 				},
 			})
