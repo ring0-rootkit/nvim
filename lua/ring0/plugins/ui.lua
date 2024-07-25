a4fd4ffa67301a010e278de513286a2b8ec245dd
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-07-25 12:01:22 +0300

    feat:lol

--- lua/ring0/plugins/ui.lua
+++ lua/ring0/plugins/ui.lua
@@ -4,7 +4,7 @@ return {
 		name = "catppuccin",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
+			vim.cmd.colorscheme("catppuccin-latte")
 		end,
 	},
 	{
