64e1967df92a202eac4e734eabab2885460e6d23
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-08-29 23:17:22 +0300

    feat: add multiple cursors

---
+++ lua/ring0/plugins/multicursor.lua
@@ -0,0 +1,42 @@
+return {
+	"brenton-leighton/multiple-cursors.nvim",
+	version = "*", -- Use the latest tagged version
+	opts = {}, -- This causes the plugin setup function to be called
+	keys = {
+		{ "<C-A-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" }, desc = "Add cursor and move down" },
+		{ "<C-A-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" }, desc = "Add cursor and move up" },
+
+		{ "<C-A-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
+		{
+			"<C-A-Down>",
+			"<Cmd>MultipleCursorsAddDown<CR>",
+			mode = { "n", "i", "x" },
+			desc = "Add cursor and move down",
+		},
+
+		{
+			"<C-LeftMouse>",
+			"<Cmd>MultipleCursorsMouseAddDelete<CR>",
+			mode = { "n", "i" },
+			desc = "Add or remove cursor",
+		},
+
+		{ "<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" }, desc = "Add cursors to cword" },
+		{
+			"<Leader>A",
+			"<Cmd>MultipleCursorsAddMatchesV<CR>",
+			mode = { "n", "x" },
+			desc = "Add cursors to cword in previous area",
+		},
+
+		{
+			"<Leader>d",
+			"<Cmd>MultipleCursorsAddJumpNextMatch<CR>",
+			mode = { "n", "x" },
+			desc = "Add cursor and jump to next cword",
+		},
+		{ "<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Jump to next cword" },
+
+		{ "<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = { "n", "x" }, desc = "Lock virtual cursors" },
+	},
+}
