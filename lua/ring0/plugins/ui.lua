64541861e40b38dee29ae5ec5b2a6c8fa43b97b8
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-09-01 12:07:52 +0300

    dark dark dark

--- lua/ring0/plugins/ui.lua
+++ lua/ring0/plugins/ui.lua
@@ -8,7 +8,7 @@ return {
 		name = "catppuccin",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
-			vim.cmd.colorscheme("catppuccin-latte")
+			vim.cmd.colorscheme("gruber-darker")
 		end,
 	},
 	{
