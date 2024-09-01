00d4ff5d8cc7fc7ab26fddf91b33847600fc8e78
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-09-01 18:48:23 +0300

    feat: less plugins

--- lua/ring0/plugins/ui.lua
+++ lua/ring0/plugins/ui.lua
@@ -1,28 +1,27 @@
 return {
+	-- {
+	-- 	"rose-pine/neovim",
+  -- "catppuccin/nvim",
+	-- },
 	{
-		"rose-pine/neovim",
 		"blazkowolf/gruber-darker.nvim",
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			vim.cmd.colorscheme("gruber-darker")
 		end,
 	},
-	{
-		"catppuccin",
-		opts = {
-			transparent_background = true,
-		},
-	},
-	{ -- Highlight todo, notes, etc in comments
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = true },
-	},
+	-- {
+	-- 	"catppuccin",
+	-- 	opts = {
+	-- 		transparent_background = true,
+	-- 	},
+	-- },
+	-- { -- Highlight todo, notes, etc in comments
+	-- 	"folke/todo-comments.nvim",
+	-- 	event = "VimEnter",
+	-- 	dependencies = { "nvim-lua/plenary.nvim" },
+	-- 	opts = { signs = true },
+	-- },
 	{
 		"stevearc/oil.nvim",
 		opts = {},
