c3de05921c24516ec32bcf28d83717373381ae25
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-09-08 22:20:35 +0300

    dark theme 4 life

--- lua/ring0/plugins/ui.lua
+++ lua/ring0/plugins/ui.lua
@@ -8,7 +8,7 @@ return {
 		"rose-pine/neovim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
-			vim.cmd.colorscheme("rose-pine-dawn")
+			vim.cmd.colorscheme("gruber-darker")
 		end,
 	},
 	{
