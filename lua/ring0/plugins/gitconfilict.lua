3c74b877264f72191bbc9689d4b5c730f9badc61
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-09-21 12:59:48 +0300

    feat: move to git fugitive

--- lua/ring0/plugins/gitconfilict.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"akinsho/git-conflict.nvim",
-	version = "*",
-	config = { default_mappings = false },
-	init = function()
-		map("n", "[x", ":GitConflictPrevConflict<CR>")
-		map("n", "]x", ":GitConflictNextConflict<CR>")
-	end,
-}
