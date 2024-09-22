578dc448f79224dec4875f366ada482d0bb875b6
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-09-22 22:41:53 +0300

    back to dark side

--- lua/ring0/plugins/ui.lua
+++ lua/ring0/plugins/ui.lua
@@ -9,7 +9,7 @@ return {
 		"rose-pine/neovim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
-			vim.cmd.colorscheme("ring0light")
+			vim.cmd.colorscheme("gruber-darker")
 		end,
 	},
 	-- {
