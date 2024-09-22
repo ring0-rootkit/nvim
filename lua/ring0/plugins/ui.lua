e9c19a47a5252fb29e7c06a985127b3c0746d68d
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-09-22 13:37:57 +0300

    lol light i m stupid

--- lua/ring0/plugins/ui.lua
+++ lua/ring0/plugins/ui.lua
@@ -2,13 +2,14 @@ return {
 	{
 		"catppuccin/nvim",
 		"blazkowolf/gruber-darker.nvim",
+		"ring0-rootkit/ring0-light.nvim",
 	},
 	{
 		-- name = "catppuccin",
 		"rose-pine/neovim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
-			vim.cmd.colorscheme("gruber-darker")
+			vim.cmd.colorscheme("ring0light")
 		end,
 	},
 	-- {
