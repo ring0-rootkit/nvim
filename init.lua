f363ad8421eeacc8a09da6c9261d7b7a970236d0
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2025-09-11 10:38:46 +0300

    rose pine setup

--- init.lua
+++ init.lua
@@ -13,10 +13,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.cmd("autocmd BufEnter * set formatoptions-=cro")
 vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
 
-vim.cmd(":syntax off")
-vim.cmd(':highlight Statement   ctermfg=magenta')     -- any statement
-vim.cmd(':highlight Conditional ctermfg=magenta')   -- if, then, else, endif, switch, etc.
-vim.cmd(':highlight Repeat      ctermfg=magenta')        -- for, do, while, etc.
-vim.cmd(':highlight Label       ctermfg=magenta')         -- case, default, etc.
-vim.cmd(':highlight Keyword     ctermfg=magenta')       -- any other keyword
-vim.cmd(':highlight Exception   ctermfg=magenta')     -- try, catch, throw
+--vim.cmd(":syntax off")
+vim.cmd(':highlight Statement   ctermfg=LightRed')     -- any statement
+vim.cmd(':highlight Conditional ctermfg=LightRed')   -- if, then, else, endif, switch, etc.
+vim.cmd(':highlight Repeat      ctermfg=LightRed')        -- for, do, while, etc.
+vim.cmd(':highlight Label       ctermfg=LightRed')         -- case, default, etc.
+vim.cmd(':highlight Keyword     ctermfg=LightRed')       -- any other keyword
+vim.cmd(':highlight Exception   ctermfg=LightRed')     -- try, catch, throw
