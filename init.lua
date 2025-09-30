1d68b2326528b8694f2112971e349080fecb6f99
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2025-09-30 18:06:34 +0300

    feat: comming to a dark side with lsp and autocompletion

--- init.lua
+++ init.lua
@@ -47,6 +47,7 @@ vim.opt.scrolloff = 10
 vim.o.shiftwidth = 8
 vim.o.tabstop = 8
 vim.o.swapfile = false
+vim.o.omnifunc = "v:lua.vim.lsp.omnifunc"
 
 vim.o.foldmethod = "marker"
 vim.o.foldlevel = 0
@@ -150,6 +151,14 @@ vim.pack.add({
 	"https://github.com/nvim-tree/nvim-web-devicons",
 	"https://github.com/stevearc/oil.nvim",
 	"https://github.com/ring0-rootkit/gitblame.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/williamboman/mason.nvim",
+	"https://github.com/williamboman/mason-lspconfig.nvim",
+	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
+	"https://github.com/hrsh7th/nvim-cmp",
+	"https://github.com/hrsh7th/cmp-nvim-lsp",
+	"https://github.com/hrsh7th/cmp-path",
+	"https://github.com/onsails/lspkind.nvim",
 })
 
 compile = require("compile-mode")
@@ -158,6 +167,7 @@ map("n", "<leader>c", compile.compile_setup)
 map("n", "<leader>r", compile.compile)
 
 require("gitblame").setup()
+require("mason").setup()
 require("oil").setup()
 require("telescope").setup({
 	defaults = {
@@ -195,3 +205,187 @@ map("n", "<leader><leader>", builtin.buffers)
 map("n", "<leader>/", function()
 	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown())
 end)
+-- }}}
+-- COMPLETION {{{
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+
+cmp.setup({
+	completion = { autocomplete = false, completeopt = "menuone,noinsert,fuzzy" },
+	window = { completion = { border = "rounded" }, documentation = { border = "rounded" } },
+	preselect = false,
+
+	mapping = cmp.mapping.preset.insert({
+		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+
+		-- If you prefer more traditional completion keymaps,
+		-- you can uncomment the following lines
+		--['<CR>'] = cmp.mapping.confirm { select = true },
+		--['<Tab>'] = cmp.mapping.select_next_item(),
+		--['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+		["<C-Space>"] = cmp.mapping.complete({}),
+	}),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "path" },
+		{ name = "supermaven" },
+	},
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol", -- show only symbol annotations
+			maxwidth = {
+				menu = 50, -- leading text (labelDetails)
+				abbr = 50, -- actual suggestion item
+			},
+			ellipsis_char = "...",
+			show_labelDetails = true,
+			symbol_map = { Supermaven = "" },
+		}),
+	},
+})
+-- }}}
+-- LSP {{{
+-- LSP Configuration & Plugins
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+	callback = function(event)
+		map("n", "gd", require("telescope.builtin").lsp_definitions)
+		map("n", "gr", require("telescope.builtin").lsp_references)
+		map("n", "gI", require("telescope.builtin").lsp_implementations)
+		map("n", "<leader>D", require("telescope.builtin").lsp_type_definitions)
+		map("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols)
+		map("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols)
+		map("n", "<leader>rn", vim.lsp.buf.rename)
+		map("n", "<leader>ca", vim.lsp.buf.code_action)
+		map("n", "K", vim.lsp.buf.hover)
+		map("n", "<C-k>", vim.lsp.buf.signature_help)
+		map("n", "gD", vim.lsp.buf.declaration)
+		map("n", "<leader>ee", "oif err != nil {<CR>}<esc>ko")
+
+		-- disable syntax highlighting
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+		client.server_capabilities.semanticTokensProvider = nil
+	end,
+})
+
+-- local border = "rounded"
+
+-- LSP settings (for overriding per client)
+-- local handlers = {
+-- 	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+-- 	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+-- }
+
+-- require("lspconfig.ui.windows").default_options = {
+-- 	border = border,
+-- }
+
+vim.diagnostic.config({
+	-- update_in_insert = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = border,
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+})
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = false
+capabilities.signatureHelpProvider = false
+
+capabilities.semanticTokensProvider = nil
+
+local servers = {
+	-- clangd = {},
+	gopls = {
+		settings = {
+			gopls = {
+				hints = {
+					assignVariableTypes = true,
+					compositeLiteralFields = true,
+					compositeLiteralTypes = true,
+					constantValues = true,
+					functionTypeParameters = true,
+					parameterNames = true,
+					rangeVariableTypes = true,
+				},
+				-- analyses = {
+				-- 	fieldalignment = true,
+				-- 	nilness = true,
+				-- 	unusedparams = true,
+				-- 	unusedwrite = true,
+				-- 	useany = true,
+				-- },
+				codelenses = {
+					gc_details = false,
+					generate = true,
+					regenerate_cgo = true,
+					run_govulncheck = true,
+					test = true,
+					tidy = true,
+					upgrade_dependency = true,
+					vendor = true,
+				},
+			},
+		},
+	},
+	rust_analyzer = {},
+	ts_ls = {},
+	zls = {
+		settings = {
+			zls = {
+				enable_inlay_hints = true,
+				enable_snippets = true,
+				inlay_hints_show_variable_type_hints = true,
+				inlay_hints_show_struct_literal_field_type = true,
+				inlay_hints_show_parameter_name = true,
+				inlay_hints_show_builtin = true,
+				warn_style = true,
+
+				inlay_hints_exclude_single_argument = false,
+				enable_argument_placeholders = false,
+			},
+		},
+	},
+
+	lua_ls = {
+		settings = {
+			Lua = {
+				completion = {
+					callSnippet = "Replace",
+				},
+				diagnostics = { disable = { "missing-fields" } },
+			},
+		},
+	},
+}
+require("mason").setup()
+
+local ensure_installed = vim.tbl_keys(servers or {})
+vim.list_extend(ensure_installed, {
+	"stylua", -- Used to format Lua code
+})
+require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+require("mason-lspconfig").setup({
+	handlers = {
+		function(server_name)
+			local server = servers[server_name] or {}
+			server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+			server.handlers = handlers
+			require("lspconfig")[server_name].setup(server)
+		end,
+	},
+})
+--}}}
